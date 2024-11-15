7b097e7fd3cad1b115f660d50f2e02019b5669f2
stlim0730 <seongtaek.lim0730@gmail.com>
2024-11-14 16:33:26 -0800

    add basic responsive css

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
 	server: {
 		port: Number(process.env.PORT),
 		host: true,
-	}
+	},
+	integrations: [icon({
+		iconDir: "src/assets/icons",
+	})],
 });
